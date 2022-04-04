d429ee5bac45459bd55c7fd74b634c612b44d873
pingliprogram2008 <76647922+pingliprogram2008@users.noreply.github.com>
2022-04-04 22:42:36 +0800

    Update onlinecheck.js

--- onlinecheck.js
+++ onlinecheck.js
@@ -1,6 +1,6 @@
 	function handin(){        
 	var password=document.getElementById("password").value;	
-	if(password=='20220430xyz'){window.alert("密码正确！");location="choose.html"}	
-	else if(password=='123abc'){window.alert("密码是20220430xyz")}	
+	if(password=='20220430wxt'){window.alert("密码正确！");location="choose.html"}	
+	else if(password=='123abc'){window.alert("密码是20220430wxt")}	
 	else {window.alert("密码错误")}	
 	              }
