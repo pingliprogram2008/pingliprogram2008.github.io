6e5d456ea2f891b869dc1f1a328fa9f720b34f78
pingliprogram2008 <76647922+pingliprogram2008@users.noreply.github.com>
2022-04-08 23:01:20 +0800

    Update and rename onlinecheck.js to onlinecheck2.js

---
+++ onlinecheck2.js
@@ -0,0 +1,6 @@
+	function handin(){        
+	var password=document.getElementById("password").value;	
+	if(password=='20220430wxt'){window.alert("授权码正确！");location="choose.html"}	
+	else if(password=='123abc'){window.alert("授权码是20220430wxt")}	
+	else {window.alert("密码错误")}	
+	              }
