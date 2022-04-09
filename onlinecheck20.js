b69e7fdd365daa806e1496de61b2b0cfc1520d84
pingliprogram2008 <76647922+pingliprogram2008@users.noreply.github.com>
2022-04-09 08:08:10 +0800

    Update and rename onlinecheck2.js to onlinecheck20.js

--- onlinecheck20.js
+++ onlinecheck20.js
@@ -1,4 +1,5 @@
-	function handin(){        
+document.getElementById("tips").value="平利软件 在线服务中"
+        function handin(){        
 	var password=document.getElementById("password").value;	
 	if(password=='20220430wxt'){window.alert("授权码正确！");location="choose.html"}	
 	else if(password=='123abc'){window.alert("授权码是20220430wxt")}	
