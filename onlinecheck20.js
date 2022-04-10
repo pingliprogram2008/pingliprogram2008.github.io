3422c1fa007a1208203c57a43faedd2faaf3fe35
pingliprogram2008 <76647922+pingliprogram2008@users.noreply.github.com>
2022-04-10 15:39:41 +0800

    Update onlinecheck20.js

--- onlinecheck20.js
+++ onlinecheck20.js
@@ -1,7 +1,7 @@
-document.getElementById("tips").value="平利软件 在线服务中"
+document.getElementById("tips").value="平利软件 在线服务中&#10;授权码是20220430wxt"
         function handin(){        
 	var password=document.getElementById("password").value;	
 	if(password=='20220430wxt'){window.alert("授权码正确！");location="choose.html"}	
-	else if(password=='123abc'){window.alert("授权码是20220430wxt")}	
+	else if(password==''){window.alert("授权码不能为空！")}	
 	else {window.alert("授权码错误")}	
 	              }
