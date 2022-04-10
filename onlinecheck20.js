40e3fa946cbd862d4088864aaffdaa3aefb670b9
pingliprogram2008 <76647922+pingliprogram2008@users.noreply.github.com>
2022-04-10 15:44:39 +0800

    Update onlinecheck20.js

--- onlinecheck20.js
+++ onlinecheck20.js
@@ -1,4 +1,4 @@
-document.getElementById("tips").value="平利软件 在线服务中&#10;授权码是20220430wxt"
+document.getElementById("tips").value="平利软件 在线服务中&#13;授权码是20220430wxt"
         function handin(){        
 	var password=document.getElementById("password").value;	
 	if(password=='20220430wxt'){window.alert("授权码正确！");location="choose.html"}	
