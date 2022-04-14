8344c0c207c7a43073c42926ef73bb4e3ca9f4eb
pingliprogram2008 <76647922+pingliprogram2008@users.noreply.github.com>
2022-04-14 23:03:02 +0800

    Update online2.0.js

--- online2.0.js
+++ online2.0.js
@@ -1,4 +1,4 @@
-document.getElementById("tips").innerHTML="平利软件 在线服务中&#10;授权码是20220430wxt"
+document.getElementById("tips").innerHTML="平利软件 在线服务中&#10;授权码是20220430wxt&#10;祝灿阳生日快乐！【比心】"
         function handin(){        
 	var password=document.getElementById("password").value;	
 	if(password=='20220430wxt'){window.alert("授权码正确！");location="choose.html"}	
