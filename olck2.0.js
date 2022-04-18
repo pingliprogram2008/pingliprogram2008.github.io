29db648df44fc623b38ca37539c070068b908791
pingliprogram2008 <76647922+pingliprogram2008@users.noreply.github.com>
2022-04-18 23:43:54 +0800

    Update and rename online2.0.js to olck2.0.js

--- olck2.0.js
+++ olck2.0.js
@@ -1,4 +1,5 @@
 document.getElementById("tips").innerHTML="平利软件 在线服务中&#10;免密登陆中&#10;广告位招租！&#10;广告位招租！"
+/*
 document.getElementById("olpswcheck").innerHTML="<input type=password id=password placeholder=请输入授权码><input type=button onClick=handin() value=提交><input type=button value=重置 onclick=olres()>"
         function handin(){        
 	var password=document.getElementById("password").value;	
@@ -9,3 +10,6 @@ document.getElementById("olpswcheck").innerHTML="<input type=password id=passwor
         function olres(){
 	document.getElementById("password").value=""
 	}
+*/
+// 上面隐藏
+document.getElementById("olpswcheck").innerHTML="<a href=choose.html>点击进入</a>"
